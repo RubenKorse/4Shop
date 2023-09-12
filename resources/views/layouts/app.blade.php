339279810692a36a741f77841d4fa9d5d14f4929
RubenKorse <121227805+RubenKorse@users.noreply.github.com>
2023-09-12 12:25:28 +0200

    week check 1

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,6 +12,9 @@
 
     <!-- Styles -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Libre+Baskerville&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="{{ asset('css/narrow-jumbotron.css') }}">
     <link rel="stylesheet" href="{{ asset('css/app.css') }}">
     {{-- @vite(['resources/css/app.css', 'resources/js/app.js']) --}}
@@ -20,7 +23,10 @@
 <body>
     <div class="container">
       <header class="header d-flex justify-content-between align-items-center">
-        <h3 class="text-muted"><a href="{{ route('home') }}" class="no-link">4Shop</a></h3>
+        <div class="logo">
+            <img src="img/4S-Logo-Transparent.png" alt="">
+            <h3 class="text-muted"><a href="{{ route('home') }}" class="no-link">4Shop</a></h3>
+        </div>
         <a href="{{ route('cart') }}"><img class="cart" src="{{ url('img/cart.png') }}" alt=""></a>
       </header>
 
@@ -44,7 +50,7 @@
       </main>
 
       <footer class="footer">
-        <p><strong>Mail bij vragen: <a href="mailto:winkelbeheer@scoutingrveer.nl">winkelbeheer@scoutingrveer.nl</a></strong> &middot; S.B.B.S. namens Scouting Raamsdonksveer, Kerklaan 22 4942AR Raamsdonksveer, KvK: 41100325, tel: 0162769096.</p>
+        <p><strong>Mail bij vragen: <a href="4Shop@gmail.com">4Shop@gmail.com</a></strong> &middot; Terheijdenseweg 350, 4826 AA Breda, KvK: 41100325, tel: 0162769096.</p>
       </footer>
 
     </div>
